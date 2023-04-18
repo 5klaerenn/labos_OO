8d5379acb4c605a6c92658803db1c9c462194ecc
5klaerenn <69436470+5klaerenn@users.noreply.github.com>
2023-04-18 09:37:06 -0400

    Renamed repo

---
+++ 935_OOP/src/Révisions/Exercice12.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Révisions;
+
+/**
+ *
+ * @author Sklaerenn
+ */
+public class Exercice12 {
+    
+    public static void main(String[] args) {
+        
+        /*
+        Exercice 12 : Un programme principal saisit une séquence d’ADN valide :
+        • Écrire une fonction valide () qui reçoit en paramètre une séquence et qui renvoie vrai si la
+        saisie de la séquence est valide, faux sinon.
+        o « valide » signifie que la séquence n’est pas vide et est formée exclusivement d’une
+        combinaison arbitraire de "a", "t", "g" ou "c").
+        • Écrire une méthode occurrences () qui reçoit en paramètre une séquence et qui affiche le
+        nombre d’occurrences de chaque caractère (a, t, g, c).
+        • Écrire une méthode saisie () qui demande à l’utilisateur de saisir une séquence et vérifie si
+        la séquence est valide ou non
+        o Si la séquence est valide, afficher le nombre d’occurrence de chaque caractère.
+        o Sinon, afficher un message pour d’erreur pour l’utilisateur.   
+        */
+        
+        
+        
+        
+    }
+    
+}
