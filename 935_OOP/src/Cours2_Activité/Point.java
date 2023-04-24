12cb41256c0e325cbef04559b5656e3c99d2f9d7
5klaerenn <69436470+5klaerenn@users.noreply.github.com>
2023-04-24 10:06:00 -0400

    création de la classe Point

---
+++ 935_OOP/src/Cours2_Activité/Point.java
@@ -0,0 +1,24 @@
+package Cours2_Activité;
+
+/**
+ *
+ * @author Sklaerenn
+ */
+public class Point {
+    
+    private double x;
+    private double y; 
+    
+    public Point(double a, double b)
+    {
+        x = a;
+        y = b;
+    }
+    
+    public Point()
+    {
+        x = 0;
+        y = 0;
+    }
+
+}
