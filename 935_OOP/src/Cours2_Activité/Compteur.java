8a5b4aab84414490d3b747be3f3e8f917af2d073
5klaerenn <69436470+5klaerenn@users.noreply.github.com>
2023-04-24 11:27:12 -0400

    Ajout activité création d'un compteur (création de la classe + création programme de test)

---
+++ 935_OOP/src/Cours2_Activité/Compteur.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Cours2_Activité;
+
+/**
+ *
+ * @author Sklaerenn
+ */
+public class Compteur {
+    
+    private int compteur;
+    
+    public Compteur()
+    {
+        compteur = 0;
+    }
+    
+    public Compteur(int c)
+    {
+        compteur = c;
+    }
+    
+    public void incrementer()
+    {
+        compteur = compteur + 1;
+    }
+    
+    public void decrementer()
+    {
+        compteur = compteur - 1;
+    }
+    
+    public void aZero()
+    {
+        compteur = 0;
+    }
+    
+    public void afficher()
+    {
+        System.out.println("compteur: "+ compteur);
+
+    }
+}
---
+++ 935_OOP/src/Cours2_Activité/Test_Compteur.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Cours2_Activité;
+
+/**
+ *
+ * @author Sklaerenn
+ */
+public class Test_Compteur {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Compteur cpt1 = new Compteur();
+        Compteur cpt2 = new Compteur(5);
+        
+        cpt1.afficher();
+        cpt2.afficher();
+        
+        cpt1.incrementer();
+        cpt1.incrementer();
+        cpt1.afficher();
+        
+        cpt2.aZero();
+        cpt2.incrementer();
+        cpt2.decrementer();
+        cpt2.afficher();
+        
+    }
+    
+}
