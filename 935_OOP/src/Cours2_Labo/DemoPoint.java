2c92eb446a12cbe867aaa7d0ea01056f390e3e09
5klaerenn <69436470+5klaerenn@users.noreply.github.com>
2023-05-01 08:34:52 -0400

    Ajout d'un affichage

--- 935_OOP/src/Cours2_Labo/DemoPoint.java
+++ 935_OOP/src/Cours2_Labo/DemoPoint.java
@@ -15,12 +15,17 @@ public class DemoPoint {
      */
     public static void main(String[] args) {
 
+        //Création des deux premiers points
         Point pointOrigine = new Point();
         Point point1 = new Point(4, 10);
+        
+        //Création de deux points suppl. 
         Point point2 = new Point(12, 5);
         Point point3 = new Point(8, 6);
         
+        System.out.println("Coordonnées du premier point");
         point2.afficher();
+        System.out.println("Coordonnées du second point :");
         point3.afficher();
         
         System.out.println("Nombre de points créés: "+Point.cpteur);
