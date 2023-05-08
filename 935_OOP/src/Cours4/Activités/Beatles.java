47f2440a24bc46eff1b965a34fabd3df18a1374f
5klaerenn <69436470+5klaerenn@users.noreply.github.com>
2023-05-08 08:52:43 -0400

    Ajout des fichiers d'activité

---
+++ 935_OOP/src/Cours4/Activités/Beatles.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Activites;
+
+/**
+ *
+ * @author Asma
+ */
+import java.util.Vector;
+
+public class Beatles
+{
+   public static void main (String[] args)
+   {
+      Vector<String> band = new Vector<String>();
+
+      band.addElement ("Paul");
+      band.addElement ("Pete");
+      band.addElement ("John");
+      band.addElement ("George");
+
+      System.out.println (band);
+
+      
+   }
+}
---
+++ 935_OOP/src/Cours4/Activités/Rectangle.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exemple2;
+
+/**
+ *
+ * @author Asma
+ */
+public class Rectangle {
+    
+    private int longueur;
+	private int largeur ;
+	
+	public Rectangle(int longueur, int largeur) 
+	{ this.longueur = longueur; 
+	  this.largeur = largeur;
+	}
+
+	// méthode pour calculer et retourner le périmètre
+	public int perimetre(){
+		return 2 * (longueur + largeur);
+	}
+	
+		
+	// methode pour afficher un rectangle
+	public void afficher () {
+		System.out.println( "<longueur = " + longueur + ", largeur = " + largeur + ", perimetre = "
+		+ perimetre() + ">");	
+	}
+	
+	public int getLongueur() {
+		return longueur;
+	}
+
+      public int getLargeur()  {
+            return largeur;
+     }
+    
+}
