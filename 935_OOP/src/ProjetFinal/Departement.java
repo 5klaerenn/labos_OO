3d2218e0f170a8eefbd40a388b4e51e9e494baed
Sklaerenn <69436470+5klaerenn@users.noreply.github.com>
2023-05-17 17:39:05 -0400

    Initialisation des classes pour le projet final

---
+++ 935_OOP/src/ProjetFinal/Departement.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ProjetFinal;
+
+/**
+ *
+ * @author Sklaerenn
+ */
+public class Departement {
+    
+    private String nom;
+       
+    public Departement(String n){
+        this.nom = n; 
+    }
+    
+    public String getNom(){
+        return this.nom;
+    }
+    
+    public String toString(){
+        return nom;
+    }
+}
---
+++ 935_OOP/src/ProjetFinal/Informatique.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ProjetFinal;
+
+/**
+ *
+ * @author Sklaerenn
+ */
+public class Informatique extends Departement {
+    
+    public Informatique(String n){
+        super(n);
+    }
+    
+}
