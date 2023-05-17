4f57d4b7b3d7711082a57a969bdf072b79d5dc4c
5klaerenn <69436470+5klaerenn@users.noreply.github.com>
2023-05-17 10:30:57 -0400

    Activité de casting

---
+++ 935_OOP/src/Cours5/Activités/Pomme.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Cours5.Activités;
+
+/**
+ *
+ * @author Sklaerenn
+ */
+public class Pomme extends Fruit {
+    
+    public void methode1()
+        {
+        System.out.println("Je suis une pomme");
+        }
+    
+    public void methode2()
+        {
+        System.out.println("très délicieuse...");
+        }
+
+}
